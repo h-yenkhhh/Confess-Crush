605a12d2a9e5ab3d2af3c1bd376efe9d727dc04e
h-yenkhhh <106753740+h-yenkhhh@users.noreply.github.com>
2022-06-02 21:22:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "h.yenkhhhh",
+    introTitle: 'xin ckèo Hẻi Ín',
+    introDesc: `chào mừng em đến với tình iu siu to của anh dành cho emmmm :3`,
+    btnIntro: 'gét gô',
+    title: 'chọn đúng là thưởng chọn sai cũm thưởng nèeee',
+    desc: 'em có đồng ý làm vợ anh hong ',
+    btnYes: 'có ạ :3',
+    btnNo: 'kó ạ',
+    question: 'về chung nhà cho anh bắt nạt nhooooo',
+    btnReply: 'hãy say yessss',
+    reply: 'hong bé ơi',
+    mess: 'em mà nói hong là anh lăn ra đường giãy đành đạch áaaa',
+    messDesc: 'píc ngay mà bé nói khum đúng hongggg',
+    btnAccept: 'có dou trrrr',
+    messLink: 'https://www.facebook.com/messages/t/100049783372838' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
